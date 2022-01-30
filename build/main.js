e8a27d487e16fe157e32223754a801f36297dc4a
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-01-30 17:05:04 +0800

    i forgot to build 💀

--- build/main.js
+++ build/main.js
@@ -2,8 +2,24 @@
 // github copilot shenanigans
 // Language: typescript
 // Path: src\main.ts
-var message = "Hello, world!";
-console.log(message);
 function toggle_theme() {
     document.body.classList.toggle("dark");
+    var darkMode = localStorage.getItem('darkMode');
+    switch (darkMode) {
+        case "true":
+            localStorage.setItem('darkMode', 'false');
+        case "false":
+            localStorage.setItem('darkMode', 'true');
+    }
+}
+function set_theme() {
+    var darkMode = localStorage.getItem('darkMode');
+    switch (darkMode) {
+        case "true":
+            document.body.classList.toggle("dark");
+        case "false":
+            console.log("User prefers light mode");
+        case undefined:
+            localStorage.setItem('darkMode', 'false');
+    }
 }
