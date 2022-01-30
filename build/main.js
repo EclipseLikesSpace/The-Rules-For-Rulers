ed9ad2e4c95acb64794e3006ed47d58d8f74ba56
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-01-30 16:54:19 +0800

    dark theme functionality

--- build/main.js
+++ build/main.js
@@ -1,3 +1,10 @@
 "use strict";
+// github copilot shenanigans
+// Language: typescript
+// Path: src\main.ts
 var message = "Hello, world!";
 console.log(message);
+function toggle_theme() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,8 @@
 
 let message: string = "Hello, world!"
 console.log(message)
+
+function toggle_theme(){
+    var element: any = document.body;
+    element.classList.toggle("dark-mode");
+}
