ed7b50528d9f923fb099cc3fdda62659781b1e52
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-01-30 22:25:42 +0800

    build

---
+++ build/game.js
@@ -0,0 +1,9 @@
+"use strict";
+// EVEN more github copilot shenanigans!!
+// Language: typescript
+// Path: src\main.ts
+class Key {
+    constructor(keyType) {
+        this.keyType = keyType;
+    }
+}
---
+++ build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// github copilot shenanigans!!
+// Language: typescript
+// Path: src\main.ts
+console.log("Hey, what are you doing here?");
+console.log("Watch this video, now! https://youtu.be/rStL7niR7gs");
+console.log("P.S: ily cgp grey, i dont mean to infringe any copyright. if theres any problems pls reach out to me on github");
