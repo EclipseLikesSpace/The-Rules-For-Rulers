9446501d1980fc374cb1120e3f00938ca28963f7
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-01-30 18:16:49 +0800

    fix button

--- build/main.js
+++ build/main.js
@@ -1,5 +1,5 @@
 "use strict";
-// github copilot shenanigans
+// github copilot shenanigans!!
 // Language: typescript
 // Path: src\main.ts
 function toggle_theme() {
@@ -14,11 +14,13 @@ function toggle_theme() {
 }
 function set_theme() {
     var darkMode = localStorage.getItem('darkMode');
+    var darkModeBtn = document.getElementById('dark-mode');
     localStorage.setItem('darkMode', 'false');
     switch (darkMode) {
         case "true":
             console.log("User prefers dark mode");
             document.body.classList.toggle("dark");
+            darkModeBtn.classList.toggle("dark");
             break;
         case "false":
             console.log("User prefers light mode");
--- src/main.ts
+++ src/main.ts
@@ -14,12 +14,13 @@ function toggle_theme(){
 
 function set_theme(){
     const darkMode: any = localStorage.getItem('darkMode');
+    const darkModeBtn: any = document.getElementById('dark-mode');
     localStorage.setItem('darkMode', 'false');
     switch (darkMode){
         case "true":
             console.log("User prefers dark mode")
             document.body.classList.toggle("dark");
-            document.getElementById('darkModeBtn').classList.toggle("dark");
+            darkModeBtn.classList.toggle("dark");
             break;
         case "false":
             console.log("User prefers light mode");
