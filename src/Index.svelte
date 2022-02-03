d85367715ef65a2a9912dc519fe63885dd7e1503
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-02-03 17:32:39 +0800

    Initial commit

---
+++ src/Index.svelte
@@ -0,0 +1,107 @@
+<script>
+    import Key from './Key.svelte';
+</script>
+
+<main>
+    <head>
+        <title>The Rules for Rulers</title>
+        <link rel="shortcut icon" type="image/png" href="../images/rulers4rulers_logo_transparent.png">
+    </head>
+    <body>
+        <em><h2 class="center">"Why don't rulers see as clearly as you?"</h2></em>
+        <Key id="general_test" name="General Ofsho" rank="General" path="./images/general.png"/>
+        <p id="lowerright"><a href="https://github.com/EclipseLikesSpace/The-Rules-For-Rulers" style="color: green;">Github</a></p>
+    </body>
+</main>
+
+<style src="style.css">
+    @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai+Looped:wght@200&display=swap');
+
+    * {
+        font-family: 'IBM Plex Sans Thai Looped', sans-serif;
+        font-size: medium;
+        transition-duration: 500ms;
+        cursor: url("../images/cursor.cur"),url("../images/cursor.png"),default
+    }
+
+    h1 {
+        font-size: 48px;
+        text-decoration: underline;
+        text-decoration-color: #7a7a7a;
+    }
+
+    h2 {
+        font-size: 34px;
+    }
+
+    h3 {
+        font-size: 21px;
+    }
+
+    .center-image{
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 100%;
+    }
+
+    .banner {
+        text-decoration: underline;
+        text-decoration-color: #7a7a7a;
+        text-align: center;
+    }
+
+    #lowerleft {
+        position: fixed;
+        left: 1px; bottom: 1px;
+    }
+
+    #lowerright {
+        position: fixed;
+        right: 1px; bottom: 1px;
+    }
+
+    footer {
+        background-color: #aaaaaa;
+    }
+
+    button {
+        outline: none;
+        border: 2px solid #404040;
+        background-color: transparent;
+        border-radius: 5px;
+        color: #202020;
+        size: 150px;
+        transition-duration: 500ms;
+        width: 4rem;
+        height: 4rem;
+    }
+
+    center {
+        text-align: center;
+    }
+
+    .center {
+        text-align: center;
+    }
+
+    button:hover {
+        background-color: #d3d3d3;
+    }
+
+    .dark {
+        background-color: #202020;
+        color: #f5f5f5;
+        transition-duration: 500ms;
+    }
+
+    ::-moz-selection {
+    color: rgb(255, 255, 255);
+    background: rgb(0, 0, 0);
+    }
+
+    ::selection {
+    color: rgb(255, 255, 255);
+    background: rgb(0, 0, 0);
+    }
+</style>
---
+++ src/Key.svelte
@@ -0,0 +1,51 @@
+<script>
+    export let id = null;
+    export let name = null;
+    export let rank = null;
+    export let path = null;
+</script>
+
+<div id={id} max-width="500px" class="key">
+    <img src={path} alt="key_image">
+    <h3>{rank} {name}</h3>
+    <h4><em>{rank}</em></h4>
+    <p>Hello, world!</p>
+</div>
+
+<style>
+    .key {
+        outline: none;
+        border: 2px solid #404040;
+        background-color: transparent;
+        border-radius: 5px;
+        width: 500px;
+        margin: auto;
+        transition-duration: 500ms;
+    }
+
+    .key img {
+        float: left;
+        width: 100px;
+        height: 100px;
+        background: transparent;
+        border: 2px solid #404040;
+        border-radius: 5px;
+    }
+
+    .key h3 {
+        position: relative;
+        top: -20px;
+        left: 10px;
+    }
+    .key h4 {
+        position: relative;
+        top: -55px;
+        left: 20px;
+        color: #707070;
+    }
+    .key p {
+        position: relative;
+        top: -80px;
+        left: 120px;
+    }
+</style>
